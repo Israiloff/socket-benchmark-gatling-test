23d0f14e5eb27aabc28ffe8dd10acd4adfa7ed84
Israiloff <stardustindark@gmail.com>
2024-11-13 17:27:38 +0500

    initial commit;

---
+++ src/main/java/io/github/israiloff/socket/benchmark/gatling/test/GatlingTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.israiloff.socket.benchmark.gatling.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatlingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatlingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/israiloff/socket/benchmark/gatling/test/GatlingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.israiloff.socket.benchmark.gatling.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatlingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
